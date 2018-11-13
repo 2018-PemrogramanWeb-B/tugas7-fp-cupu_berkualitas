0339a24389683c12a06701e4a9a4fadb419a0931
cluster777 <42055002+cluster777@users.noreply.github.com>
2018-11-13 12:29:58 +0700

    repair push

---
+++ validate login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+//find user name
+$conn= new mysqli("localhost","root","","mydb");
+	if ($conn->connect_error) {
+    die("Connection failed: <br>" . $conn->connect_error);
+	}
+	$user=$_POST["uname"];
+	$result=$conn->query('SELECT password FROM user WHERE name="'.$user."\"");
+	if (!$result)
+	{
+		header("location:login.php");
+	}
+	else{
+		$row=$result->fetch_assoc();
+		echo $row["password"];
+		$pass=$_POST["pswd"];
+		if(md5($pass)==($row["password"]))
+		{
+			echo"sukses";
+			$session["user"]=$user;
+			header("location:welcome.blade.php");
+		}
+	}
+	
+//not found mark as false
+//check pswd 
+
+?>
+<!DOCTYPE html>
